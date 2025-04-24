9ea8c7be3b7c6dd6a11a2dced5d111c1a7040e2d
JuliaSilva05 <jusilva.souto@gmail.com>
2025-04-24 11:23:38 -0300

    u

--- MusicalBox/src/components/MostrarInfos.jsx
+++ MusicalBox/src/components/MostrarInfos.jsx
@@ -21,7 +21,7 @@ function MostrarInfos(){
         console.log("resp ok");
         const data = await response.json();
         console.log(data);
-        console.log(data.artists);
+        console.log(data[0]);
         setArtista(data.results);
       }else {
         console.log("response status", response.status);
