8ff59cfdeb1050a471d3654c1094af759b4de364
JuliaSilva05 <jusilva.souto@gmail.com>
2025-04-24 15:57:33 -0300

    outro api

--- MusicalBox/src/components/MostrarInfos.jsx
+++ MusicalBox/src/components/MostrarInfos.jsx
@@ -1,9 +1,35 @@
 import { useState, useEffect } from 'react'
 
-const url_musica = "https://musicbrainz.org/ws/2/artist?query=gender:female%20AND%20type:person%20AND%20country:br&fmt=json&limit=5"
+//const url_musica = "https://musicbrainz.org/ws/2/artist?query=gender:female%20AND%20type:person%20AND%20country:br&fmt=json&limit=5"
+const token = "https://accounts.spotify.com/api/token"
 
 function MostrarInfos(){
-  const [artistas, setArtista] = useState([]);
+  async function fetchWebApi(endpoint, method, body) {
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method,
+      body:JSON.stringify(body)
+    });
+    return await res.json();
+  }
+  async function getTopTracks(){
+    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+    return (await fetchWebApi(
+      'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+    )).items;
+  }
+  
+  const topTracks = getTopTracks();
+  console.log(
+    topTracks?.map(
+      ({name, artists}) =>
+        `${name} by ${artists.map(artist => artist.name).join(', ')}`
+    )
+  );
+  //const [artistas, setArtista] = useState([]);
+  
 
 /*
 
@@ -12,8 +38,6 @@ function MostrarInfos(){
     .then(response => response.json())
     .then(data => setArtista(data.results))
   },[])
-
-*/
 
   async function carregar(url){
     try {
@@ -52,17 +76,14 @@ function MostrarInfos(){
           {artista,artistas}
     )
   });
+  */
 
 
   return (
     <>
       <button onClick={handleBtCarregarClick}>Carregar</button>
       <ol>
-        {artistas.map((artista) => (
-          <li key={artista.id}>
-            {artista.name}
-          </li>
-        ))}
+        oi
       </ol>
     </>
   )
