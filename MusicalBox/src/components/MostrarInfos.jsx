e0db60ea78f8693fd17104c4078d5ee3f984d9f5
JuliaSilva05 <jusilva.souto@gmail.com>
2025-04-29 08:32:22 -0300

    ola

--- MusicalBox/src/components/MostrarInfos.jsx
+++ MusicalBox/src/components/MostrarInfos.jsx
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react'
 
-//const url_albums = "https://taylor-swift-api.sarbo.workers.dev/albums"
+const url_albums = "https://taylor-swift-api.sarbo.workers.dev/albums"
 
 function MostrarInfos(){
   const [albums, setAlbums] = useState([])
 
   useEffect(() => {
     // Carregar álbuns
-    fetch('https://taylor-swift-api.sarbo.workers.dev/albums')
-      .then(response => response.json())
-      .then(data => setAlbums(data.results))
-
+    carregarAlbums()
   }, [])
+
+  async function getAlbums() {
+    try {
+      const response = await axios.get(url_albums);
+      return response.data.results || [];
+    } catch (err) {
+      console.error("Erro ao buscar álbuns:", err);
+      return [];
+    }
+  }
+  async function carregarAlbums(params) {
+    const albums = await getAlbums();
+    setAlbums(albums)
+  }
 
 
   return (
     <>
-      {/*<button onClick={handleBtCarregarClick}>Carregar</button>*/}
+      <button onClick={carregarAlbums}>Carregar</button>
       <ol>
         {albums.map((album)=>(
           <li>
