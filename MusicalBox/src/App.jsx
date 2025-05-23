d55acaa01eb3f2198bfca425cc98fe77a55b7eaa
JuliaSilva05 <jusilva.souto@gmail.com>
2025-05-22 21:05:34 -0300

    g

--- MusicalBox/src/App.jsx
+++ MusicalBox/src/App.jsx
@@ -3,16 +3,10 @@ import './App.css'
 import MostrarInfos from './components/MostrarInfos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>PopBoxd</h1>
 
       <MostrarInfos/>
       
--- MusicalBox/src/components/MostrarInfos.jsx
+++ MusicalBox/src/components/MostrarInfos.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const url_albums = "https://taylor-swift-api.sarbo.workers.dev/albums"
 
-function MostrarInfos(){
+function MostrarAlbums(){
   const [albums, setAlbums] = useState([])
     useEffect(() => {
       fetch('https://taylor-swift-api.sarbo.workers.dev/albums')
@@ -15,54 +15,19 @@ function MostrarInfos(){
         });
     }, []);
 
-  /*
-  useEffect(() => {
-    console.log("useEffect")
-    // Carregar álbuns
-    carregarAlbums()
-  }, [])
-
-  async function getAlbums() {
-    console.log("getAlbums")
-    try {
-      const response = await fetch(url_albums);
-      console.log("response:")
-      console.log(response)
-      return [response];
-    } catch (err) {
-      console.error("Erro ao buscar álbuns:", err);
-      return [];
-    }
-  }
-  async function carregarAlbums() {
-    console.log("carregarAlbums")
-    const albums = await getAlbums().then;
-    setAlbums(albums)
-  }
-*/
-
   return (
     <>
-      <button onClick={useEffect}>Carregar</button>
       <ol>
         {albums.map((album) => (
           <li key={album.album_id}>
             {album.title} ({album.release_date})
+            <li>oi</li>
           </li>
         ))}
       </ol>
-    {/*
-      <button onClick={carregarAlbums}>Carregar</button>
-      <ol>
-        {albums.map((album)=>(
-          <li key={album.album_id}>
-            {album.title} ({album.release_date})
-          </li>
-        ))}
-      </ol>
-      */}
+      
     </>
   )
 }
 
-export default MostrarInfos;
+export default MostrarAlbums;
