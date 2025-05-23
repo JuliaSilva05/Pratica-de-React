66f27ee0c36426234d50bc4a955c2ca447be3441
JuliaSilva05 <jusilva.souto@gmail.com>
2025-05-22 21:26:40 -0300

    j

--- MusicalBox/src/components/MostrarInfos.jsx
+++ MusicalBox/src/components/MostrarInfos.jsx
@@ -4,24 +4,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function MostrarAlbums(){
   const [albums, setAlbums] = useState([])
+  const [songs, setSongs] = useState([])
+
     useEffect(() => {
       fetch('https://taylor-swift-api.sarbo.workers.dev/albums')
         .then((res) => {
           return res.json();
         })
-        .then((data) => {
-          console.log(data);
-          setAlbums(data);
+        .then((albums_data) => {
+          console.log(albums_data);
+          setAlbums(albums_data);
         });
+      
 
     }, []);
+  async function concatenar(){
+
+  }
 
   return (
     <>
       <ol>
         {albums.map((album) => (
           <li key={album.album_id}>
-            {album.title} ({album.release_date})
+            {album.title} ({album.release_date}) {album.album_id}
             <ul>{album.album_id}</ul>
           </li>
         ))}
